6c75bcc9c038128a48cd031d075e65752eddcc41
Jose Torres <jt0rres7@byu.edu>
2026-03-07 16:59:07 -0700

    Pass all baseline tests

--- Algorithms/project-alignment/alignment.py
+++ Algorithms/project-alignment/alignment.py
@@ -19,8 +19,82 @@ def align(
     :param banded_width: banded_width * 2 + 1 is the width of the banded alignment; -1 indicates full alignment
     :param gap: the character to use to represent gaps in the alignment strings
     """
+    m = len(seq1)
+    n = len(seq2)
 
-    return 0, "", ""
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    trace =  [[None]*(m+1) for _ in range(n+1)]
+
+    for j in range(1, m + 1):
+        dp[0][j] = j * indel_penalty
+        trace[0][j] = "L"
+
+    for i in range(1, n + 1):
+        dp[i][0] = i * indel_penalty
+        trace[i][0] = "U"
+
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            compute_cell(dp, trace, seq1, seq2, i, j, match_award, indel_penalty, sub_penalty)
+
+    complete_seq1 = []
+    complete_seq2 = []
+    i = n
+    j = m
+    while i > 0 or j > 0:
+        if trace[i][j] == "D":
+            complete_seq1.append(seq1[j-1])
+            complete_seq2.append(seq2[i-1])
+            i = i - 1
+            j = j - 1
+        elif trace[i][j] == "U":
+            complete_seq1.append(gap)
+            complete_seq2.append(seq2[i-1])
+            i = i - 1
+        elif trace[i][j] == "L":
+            complete_seq1.append(seq1[j-1])
+            complete_seq2.append(gap)
+            j = j - 1
+
+    complete_seq1 = "".join(reversed(complete_seq1))
+    complete_seq2 = "".join(reversed(complete_seq2))
+    cost = dp[n][m]
+
+    return cost, complete_seq1, complete_seq2
+
+
+def compute_cell(
+        dp: list[list[int]],
+        trace: list[list[str | None]],
+        seq1: str,
+        seq2: str,
+        i: int,
+        j: int,
+        match_award: int,
+        indel_penalty: int,
+        sub_penalty: int,
+        gap= "-"
+) -> None:
+    if seq1[j - 1] == seq2[i - 1]:
+        diff = match_award
+    else:
+        diff = sub_penalty
+
+    diag = diff + dp[i-1][j-1]
+    up = indel_penalty + dp[i-1][j]
+    left = indel_penalty + dp[i][j-1]
+
+    best = min(diag,up,left)
+
+    dp[i][j] = best
+
+    if best == diag:
+        trace[i][j] = "D"
+    elif best == up:
+        trace[i][j] = "U"
+    else:
+        trace[i][j] = "L"
 
 
 def local_align(
